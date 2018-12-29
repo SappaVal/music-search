129ddb426811138c9e7433d5bbf781fa17ddf776
Sappa Valentin <sappavalentin@gmail.com>
2018-12-30 00:27:52 +0100

    Debut de la gestion des playlists

--- src/components/Gestion.vue
+++ src/components/Gestion.vue
@@ -1,7 +1,12 @@
 <template>
     <div class="gestion">
         <h1>Gestionnaire de vidéos</h1>
-        <p></p>
+        Mes playlists :         
+        <ul id="example-2">
+          <li v-for="(item, index) in playlists" :key="index.playlist">
+            <a href="#">{{ item.playlist.nom }} - {{ Object.keys( item.playlist.videos ).length }} vidéo(s)</a>
+          </li>
+        </ul>
     </div>
 </template>
 
@@ -9,6 +14,29 @@
 export default {
   data(){
     return {
+      playlists: [
+        { 
+          playlist: {
+            nom : 'Guitare', 
+            videos : 
+            [
+              'lien1',
+              'lien2',
+              'lien3'
+            ] 
+          }
+        },
+        { 
+          playlist: {
+            nom : 'Batterie', 
+            videos : 
+            [
+              'lien1',
+              'lien2'
+            ] 
+          }
+        }
+      ]
     }
   }
 }
