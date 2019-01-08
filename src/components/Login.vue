24b3e57ee95aa8bfe9e6fd9abba6f23439cd9c6c
Alexandre Ciarafoni <alexandre.ciarafoni@etu.unice.fr>
2019-01-08 09:00:24 +0100

    Ajout authentification utilisateur

---
+++ src/components/Login.vue
@@ -0,0 +1,51 @@
+<template>
+    <div class="login">
+        <h3>Connectez-vous</h3>
+        <md-field md-clearable>
+            <label>Email</label>
+            <md-input v-model="email"></md-input>
+        </md-field>
+        <md-field>
+            <label>Mot de passe</label>
+            <md-input v-model="mdp" type="password"></md-input>
+        </md-field>
+        <md-field>
+            <md-button class="md-raised md-primary" v-on:click="login">Connexion</md-button>
+            <p>Pas encore de compte ? <router-link to="/signUp">Inscrivez-vous</router-link></p>
+        </md-field>
+    </div>
+</template>
+
+<script>
+    import * as firebase from 'firebase/app';
+    import 'firebase/database';
+
+    export default {
+        name: 'login',
+        data() {
+            return {
+                email: '',
+                mdp: ''
+            }
+        },
+        methods: {
+            login: function() {
+                firebase.auth().signInWithEmailAndPassword(this.email, this.mdp).then(
+                    (user) => {
+                        this.$router.replace('accueil')
+                    },
+                    (err) => {
+                        alert('Dommage... ' + err.message)
+                    }
+                );
+            }
+        }
+    }
+</script>
+
+<style>
+    p {
+        margin-top: 20px;
+        font-size: 13px;
+    }
+</style>
---
+++ src/components/SignUp.vue
@@ -0,0 +1,51 @@
+<template>
+    <div class="signup">
+        <h3>Inscrivez-vous</h3>
+        <md-field md-clearable>
+            <label>Email</label>
+            <md-input v-model="email"></md-input>
+        </md-field>
+        <md-field>
+            <label>Mot de passe</label>
+            <md-input v-model="mdp" type="password"></md-input>
+        </md-field>
+        <md-field>
+            <md-button class="md-raised md-primary" v-on:click="signUp">Valider</md-button>
+            <span class="md-helper-text">Retour à la <router-link to="/">connexion</router-link></span>
+        </md-field>
+    </div>
+</template>
+
+<script>
+    import * as firebase from 'firebase/app';
+    import 'firebase/database';
+
+    export default {
+        name: 'signUp',
+        data() {
+            return {
+                email: '',
+                mdp: ''
+            }
+        },
+        methods: {
+            signUp: function() {
+                firebase.auth().createUserWithEmailAndPassword(this.email, this.mdp).then(
+                    (user) => {
+                        alert('Compte créé !')
+                        this.$router.replace('accueil')
+                    },
+                    (err) => {
+                        alert('Essaye encore ... ' + err.message)
+                    }
+                );
+            }
+        }
+    }
+</script>
+
+<style scoped>
+    div {
+        width: 350px;
+    }
+</style>
