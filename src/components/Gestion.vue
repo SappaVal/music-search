51054e8061d5768e1776559c77b5cb2f3c76d67f
Sappa Valentin <sappavalentin@gmail.com>
2019-02-04 09:38:46 +0100

    Fix page Gestion

--- src/components/Gestion.vue
+++ src/components/Gestion.vue
@@ -3,43 +3,35 @@
       <nav>
         <router-link to="/">Accueil</router-link>
         <router-link to="/gestion">Gestionnaire de vidéos</router-link>
+        <div id="example-1">
+        </div>
       </nav>
         <h1>Gestionnaire de vidéos</h1>
         Mes playlists :       
           <ul>
-            <li v-for="(item, index) in JSON.parse(JSON.stringify(this.playlists))" :key="index">              
-              <a href="#" v-on:click="openVideos(item.videos.value.id)">{{ item.nom }} - {{ Object.keys( item.videos ).length }} vidéo(s)</a>
+            <li v-for="(item, index) in JSON.parse(JSON.stringify(this.existingPlaylists[0].childSnapshot))" :key="index">              
+              <a href="#" v-on:click="openVideos(item.videos)">{{ item.nom }} - {{ Object.keys( item.videos ).length }} vidéo(s)</a>
             </li>
           </ul>
-        <div v-for="video in videosIds" 
-                :video="video"
-                :key="video">
-            <div class="card-expansion">
-                <md-card>
+          <b-container>
+            <b-row>
+              <b-col v-for="video in videosIds" :video="video" :key="video.id">
+                <div class="card-expansion">
+                  <md-card>
                     <md-card-media>
-                        <youtube v-bind:video-id="video" />
+                      <youtube v-bind:video-id="video.id"/>
                     </md-card-media>
 
                     <md-card-header>
                       <div class="md-title">{{ video.title }}</div>
                       <div class="md-subhead">{{ video.channel }}</div>
                     </md-card-header>
-                    <md-card-expand>
-                      <md-card-actions>
-                        <md-card-expand-trigger>
-                          <md-button>Description</md-button>
-                        </md-card-expand-trigger>
-                      </md-card-actions>
-
-                      <md-card-expand-content>
-                          <md-card-content>
-                              {{ video.desc }}
-                          </md-card-content>
-                      </md-card-expand-content>
-                    </md-card-expand>
-                </md-card>
-            </div>
-        </div>
+                    
+                  </md-card>
+                </div>
+              </b-col>
+            </b-row>
+          </b-container>
     </div>
 </template>
 
@@ -51,20 +43,28 @@ export default {
   data(){
     return {
       videosIds : null,
-      playlists: null,
-      playlistObserver: null
+      existingPlaylists: null
     }
   },
   created()
   {
-    this.playlistObserver = firebase.database().ref('utilisateurs/' + firebase.auth().currentUser.uid).child('playlists');
-    this.playlistObserver.on('value', (snap) => {
-      this.playlists = snap.val();
-    });
+    var query = firebase
+        .database()
+        .ref("utilisateurs/" + firebase.auth().currentUser.uid)
+        .orderByKey();
+      query.once("value").then(snapshot => {
+        this.existingPlaylists = [];
+        snapshot.forEach(childSnapshot => {
+          this.existingPlaylists.push({ childSnapshot });
+        });
+      });
   },
   methods:{
     openVideos(videos) {
       this.videosIds = videos;
+    },
+    greet: function (event) {
+      console.log(JSON.parse(JSON.stringify(this.existingPlaylists[0].childSnapshot)))
     }
   }
 }
