c893fe467431fd9bebd21889afa9779cfbdd5567
Alexandre Ciarafoni <alexandre.ciarafoni@etu.unice.fr>
2019-02-25 16:27:04 +0100

    Ajout de la modification de playlist : vidéo d'une playlist à l'autre et renommage de vidéo

--- src/components/Gestion.vue
+++ src/components/Gestion.vue
@@ -1,30 +1,19 @@
 <template>
   <div class="gestion">
-      <nav>
-        <router-link to="/">Accueil</router-link>
-        <router-link to="/gestion">Gestionnaire de vidéos</router-link>
-      </nav>
-      <h1>Gestionnaire de vidéos</h1>
-      <!--<ul v-if="this.existingPlaylists !== null">
-        <li
-          v-for="(item, index) in JSON.parse(JSON.stringify(this.existingPlaylists[0].childSnapshot))"
-          :key="index"
-        >
-          <a
-            v-if="item.videos"
-            href="#"
-            v-on:click="openVideos(item.videos)"
-          >{{ item.nom }} - {{ Object.keys( item.videos ).length }} vidéo(s)</a>
-          <span v-else href="#">{{ item.nom }} - Aucune vidéo disponible</span>
-        </li>
-      </ul>-->
+    <nav>
+      <router-link to="/">Accueil</router-link>
+      <router-link to="/gestion">Gestionnaire de vidéos</router-link>
+    </nav>
+    <h1>Gestionnaire de vidéos</h1>
+
     <div class="md-layout-item">
       <md-field v-if="this.existingPlaylists !== null && this.existingPlaylists !== undefined">
         <label for="SelectedPlay">Mes playlists</label>
         <md-select v-model="SelectedPlay" name="SelectedPlay">
           <md-option
             v-for="(item, index) in JSON.parse(JSON.stringify(this.existingPlaylists[0].childSnapshot))"
-            v-bind:key="index"
+            :key="index"
+            v-bind:value="item.nom"
           >&nbsp;
             <span
               v-if="item.videos"
@@ -51,7 +40,7 @@
               </md-card-header>
 
               <div>
-                <md-button class="md-primary" @click="openPopup()">Modifier</md-button>
+                <md-button class="md-primary" @click="openPopup(index)">Modifier</md-button>
                 <md-button class="md-accent" @click="removeVideoFromPlaylist(index)">Supprimer</md-button>
               </div>
             </md-card>
@@ -78,8 +67,9 @@
                 <md-select v-model="changePlaylist" name="changePlaylist" id="changePlaylist">
                   <md-option
                     v-for="(item, index) in JSON.parse(JSON.stringify(this.existingPlaylists[0].childSnapshot))"
-                    v-bind:key="index"
-                  >{{ item.nom }}</md-option>
+                    :key="index"
+                    v-bind:value="index"
+                  >&nbsp;{{ item.nom }}</md-option>
                 </md-select>
               </md-field>
             </div>
@@ -90,8 +80,22 @@
           </md-tab>
         </md-tabs>
       </md-dialog>
+      <md-dialog-alert
+        :md-active.sync="created"
+        md-content="Element correctement modifié !"
+        md-confirm-text="Au top!"
+      />
+      <md-dialog-alert
+        :md-active.sync="failed"
+        md-content="La requête a échoué :("
+        md-confirm-text="Pas cool..."
+      />
+      <md-dialog-alert
+        :md-active.sync="wrong"
+        md-content="Il faut au moins un nouveau nom ou une nouvelle playlist!"
+        md-confirm-text="Ok!"
+      />
     </div>
-
   </div>
 </template>
 
@@ -102,12 +106,16 @@ export default {
   data() {
     return {
       SelectedPlay: null,
+      created: false,
+      failed: false,
+      wrong: false,
       videosIds: null,
       playlistId: null,
       showDialog: false,
       existingPlaylists: null,
       changePlaylist: "",
-      newVideoName: ""
+      newVideoName: "",
+      videoToMove: null
     };
   },
   created() {
@@ -127,22 +135,139 @@ export default {
       this.videosIds = videos;
       this.playlistId = pid;
     },
-    openPopup: function() {
+    openPopup: function(vidToMove) {
       this.showDialog = true;
+      this.videoToMove = vidToMove;
     },
     cancelPopup: function() {
+      document.location.reload(true);
       this.showDialog = false;
     },
     changeVideoPlaylist: function(playlist) {
+      // Fonction de modification de playlist : changement de nom d'une vidéo // Changement de playlist par vidéo
       this.changePlaylist = playlist;
-      console.log("Nouvelle playlist : " + this.changePlaylist);
+
+      // Préparation de la requête
+      var query = firebase
+        .database()
+        .ref(
+          "utilisateurs/" + firebase.auth().currentUser.uid + "/playlists/" + this.playlistId + "/videos/" + this.videoToMove
+        )
+        .orderByKey();
+
+      if(this.newVideoName !== "" && this.changePlaylist !== "")
+      {
+        // Cas de changement de nom de vidéo et changement de playlist
+        // Ajout de la vidéo modifiée
+        query.once("value").then(snapshot => {
+          firebase
+          .database()
+          .ref(
+            "utilisateurs/" +
+              firebase.auth().currentUser.uid +
+              "/playlists/" +
+              this.changePlaylist +
+              "/videos"
+          )
+          .push({
+            id: snapshot.val().id,
+            desc: snapshot.val().desc,
+            title: this.newVideoName,
+            channel: snapshot.val().channel
+          })
+          .then(
+            () => {
+              // Suppression de la vidéo non modifiée
+              firebase
+              .database()
+              .ref("utilisateurs/" + firebase.auth().currentUser.uid + "/playlists/" + this.playlistId + "/videos/" + this.videoToMove)
+              .remove();
+              this.created = true;
+            },
+            () => {
+              this.failed = true;
+            }
+          );
+        });
+
+      } else if(this.newVideoName !== "" && this.changePlaylist == "" || this.changePlaylist == this.playlistId) {
+        // Cas du changement de nom de vidéo
+        // Ajout de la vidéo modifiée
+        query.once("value").then(snapshot => {
+          firebase
+          .database()
+          .ref(
+            "utilisateurs/" +
+              firebase.auth().currentUser.uid +
+              "/playlists/" +
+              this.playlistId +
+              "/videos"
+          )
+          .push({
+            id: snapshot.val().id,
+            desc: snapshot.val().desc,
+            title: this.newVideoName,
+            channel: snapshot.val().channel
+          })
+          .then(
+            () => {
+              // Suppression de la vidéo non modifiée
+              firebase
+              .database()
+              .ref("utilisateurs/" + firebase.auth().currentUser.uid + "/playlists/" + this.playlistId + "/videos/" + this.videoToMove)
+              .remove();
+              this.created = true;
+            },
+            () => {
+              this.failed = true;
+            }
+          );
+        });
+
+      } else if(this.newVideoName == "" && this.changePlaylist !== "" && this.changePlaylist !== this.playlistId) {
+        // Cas du changement de playlist
+        // Ajout de la vidéo modifiée
+        query.once("value").then(snapshot => {
+          firebase
+          .database()
+          .ref(
+            "utilisateurs/" +
+              firebase.auth().currentUser.uid +
+              "/playlists/" +
+              this.changePlaylist +
+              "/videos"
+          )
+          .push({
+            id: snapshot.val().id,
+            desc: snapshot.val().desc,
+            title: snapshot.val().title,
+            channel: snapshot.val().channel
+          })
+          .then(
+            () => {
+              // Suppression de la vidéo non modifiée
+              firebase
+              .database()
+              .ref("utilisateurs/" + firebase.auth().currentUser.uid + "/playlists/" + this.playlistId + "/videos/" + this.videoToMove)
+              .remove();
+              this.created = true;
+            },
+            () => {
+              this.failed = true;
+            }
+          );
+        });
+
+      } else {
+        this.wrong = true;
+      }
     },
     removeVideoFromPlaylist: function(vid) {
       firebase
         .database()
         .ref("utilisateurs/" + firebase.auth().currentUser.uid + "/playlists/" + this.playlistId + "/videos/" + vid)
         .remove();
-      document.getElementById(vid).style.display = "none";
+      document.location.reload(true);
     },
     removePlaylist: function() {
       firebase
